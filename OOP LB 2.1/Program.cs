efe92417d3187861439f6c77ab12471beb432928
Andrew1929 <andre2904030@gmail.com>
2023-05-25 18:01:07 +0300

    OOP_LR_2

---
+++ OOP LB 2.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace OOP_LB_2._1
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public double Grade { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("C:/Users/Admin/Documents/students.TXT"))
+            {
+                List<Student> students = new List<Student>();
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string[] parts = line.Split(':');
+                    Student student = new Student();
+                    student.Name = parts[0];
+                    student.Grade = double.Parse(parts[1]);
+                    students.Add(student);
+                }
+                List<Student> filteredStudents = students.OrderByDescending(student => student.Grade).ToList();
+                foreach (Student student in filteredStudents)
+                {
+                    Console.WriteLine(student.Name + ": " + student.Grade);
+                }
+            }
+        }
+    }
+}
---
+++ OOP LR 2.4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP_LR_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> strings = new List<string>() { "Hello", "world", "!", "Glory", "to", "Ukraine", "!!!" };
+
+            Func<string, int> lengthDelegate = s => s.Length;
+
+            foreach (string str in strings)
+            {
+                int length = lengthDelegate(str);
+                Console.WriteLine("{0} - довжина: {1}", str, length);
+            }
+        }
+    }
+}
